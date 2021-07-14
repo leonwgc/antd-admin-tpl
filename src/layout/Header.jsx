44974d5161e78319ec2eac23401f4884c802d5de
leonwgc <giantfish@126.com>
2021-07-14 12:36:44 +0800

    feat: update header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,40 +1,31 @@
 import React from 'react';
-import { Menu, Avatar, Dropdown } from 'antd';
+import { Menu, Avatar, Dropdown, Space } from 'antd';
 import { useSelector, useUpdateStore } from 'simple-redux-store';
+import { useHistory } from 'react-router-dom';
 import { MenuUnfoldOutlined, MenuFoldOutlined, UserOutlined } from '@ant-design/icons';
 import * as storage from 'simple-browser-store';
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
   display: flex;
+  height: 48px;
   background-color: #fff;
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
-  /* box-shadow: 0 1px 4px rgb(0 21 41 / 8%); */
   border-bottom: 1px solid #f5f5f5;
 
   .folder {
-    font-size: 20px;
-    line-height: 48px;
-    cursor: pointer;
-    transition: all 0.3s ease;
+    font-size: 16px;
     color: rgba(0, 0, 0, 0.65);
-    padding: 0 24px 0 0;
     &:hover {
       color: #004bcc;
     }
   }
-  > .content {
-    cursor: pointer;
-    padding: 0 20px;
-    &:hover {
-      background: #e6f3ff;
-    }
-  }
 `;
 
-export default function PageHeader({ history, toggleCollapsed, collapsed }) {
+export default function Header() {
+  const history = useHistory();
   const { name } = storage.getData('localStorage', 'admin');
   const { menuCollapsed } = useSelector((state) => state.app);
 
@@ -56,21 +47,20 @@ export default function PageHeader({ history, toggleCollapsed, collapsed }) {
   return (
     <StyledHeader>
       <a className="folder" onClick={() => updateStore({ menuCollapsed: !menuCollapsed })}>
-        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-          style: { fontSize: 20 },
-        })}
+        {menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
       </a>
-      <div className="content">
+
+      <Space size={12}>
         <Dropdown overlay={menu}>
           <div>
             <Avatar
               icon={<UserOutlined />}
               style={{ marginRight: 8, backgroundColor: '#87d068' }}
             />
-            {name}
           </div>
         </Dropdown>
-      </div>
+        <span>{name}</span>
+      </Space>
     </StyledHeader>
   );
 }
