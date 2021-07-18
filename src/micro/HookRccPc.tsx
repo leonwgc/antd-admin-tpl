63dbadfe4ec87e154d6e5ae3357e5276d5215993
leonwgc <giantfish@126.com>
2021-07-18 13:37:51 +0800

    feat: use simple-react-qiankun

--- src/micro/HookRccPc.tsx
+++ src/micro/HookRccPc.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import QKMicroAppRender from '../common/QKMicroAppRender';
+import QKMicroAppRender from 'simple-react-qiankun';
 
 const App = () => {
   return (
-    <QKMicroAppRender
-      app={{
-        name: 'hooks-rcc-pc',
-        entry: '//localhost:3002/pc.html#/afr',
-      }}
-    />
+    <div>
+      <QKMicroAppRender
+        app={{
+          name: 'micro-pc-demo',
+          entry: '//localhost:3002/pc.html#/afr',
+        }}
+      />
+    </div>
   );
 };
 
